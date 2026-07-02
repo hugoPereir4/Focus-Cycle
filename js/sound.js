fa94971339b10c22a6f52d0f0a4577fe1aac3724
hugoPereir4 <hp971221@gmail.com>
2026-07-02 18:24:15 -0300

    feat: adiciona a função playBeep usando web audio api

--- js/sound.js
+++ js/sound.js
@@ -11,3 +11,22 @@ function getContext() {
         return audioContext;
     }
 }
+
+function playBeep(frequency, duration, type, volume) {
+    const context = getContext();
+    const oscillatorNode = context.createOscillator();
+    const gainNode = context.createGain();
+
+    oscillatorNode.connect(gainNode);
+    gainNode.connect(context.destination);
+
+    oscillatorNode.frequency.value = frequency;
+    oscillatorNode.type = type;
+    gainNode.gain.value = volume;
+
+    gainNode.gain.setValueAtTime(volume, context.currentTime);
+    gainNode.gain.linearRampToValueAtTime(0, context.currentTime + duration);
+
+    oscillatorNode.start();
+    oscillatorNode.stop(context.currentTime + duration);
+}
