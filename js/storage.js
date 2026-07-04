f414b609beb877214d4a462dee56844c8c20e05f
hugoPereir4 <hp971221@gmail.com>
2026-07-04 18:26:23 -0300

    feat: cria uma função interna que captura e retorna a data no formato YYYY-MM-DD

---
+++ js/storage.js
@@ -0,0 +1,10 @@
+function getDayKey() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+
+    return `focus-cycle-${year}-${month}-${day}`;
+}
+
+console.log(getDayKey());
