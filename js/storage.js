d06866844825a8b16b5f92fcd5b909e903852408
hugoPereir4 <hp971221@gmail.com>
2026-09-03 18:00:44 -0300

    fix: corrige o retorno da função getDayKey dentro do storage.js

--- js/storage.js
+++ js/storage.js
@@ -4,7 +4,7 @@ function getDayKey() {
     const month = String(now.getMonth() + 1).padStart(2, '0');
     const day = String(now.getDate()).padStart(2, '0');
 
-    return `focus-cycle-${year}-${month}-${day}`;
+    return `focuscycle-${year}-${month}-${day}`;
 }
 
 export function loadHistory() {
